25cfdf9ef9a54b696049ae268de99e4eb561d3a5
AchmadFals <mangkolil5@gmail.com>
2022-03-24 14:30:37 +0700

    kode terbaru

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+import crop from './crop.png';
 
 const Login=() => {
+  const [formData, setFormData] = useState({})
+
+  const handleLogin = async e =>{
+    const {username, password} = formData;
+    await axios.post("http://192.168.175.127:8000/login",{
+      username,
+      password
+    })
+  }
+  console.log("formData =>", formData)
   return (
     <div className="bg-cyan-400 min-h-screen">
       <div className="row">
@@ -9,22 +21,32 @@ const Login=() => {
       </div>
       <div className="row w-1/3 bg-white shadow-lg shadow-sky-800 text-center m-auto rounded-xl py-3.5">
         <div className="form px-6 py-4">
-          <img src="https://www.pinclipart.com/picdir/middle/387-3873569_catalyst-react-js-logo-svg-clipart.png" />
-          <div className="form-body">
+          <img src={crop} className="h-24 ml-36" />
+          <div className="form-body p-7">
               <div className="username pb-2">
-                <label className="form__label block flex font-bold text-emerald-500" for="username">Username</label>
-                <input className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" type="text" id="userName" placeholder="Username"/>
+                <label className="form__label block flex font-medium text-neutral-500">Username</label>
+                <input onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    username: e.target.value
+                  })
+                }} className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" type="text" id="userName" placeholder="Username"/>
               </div>
               <div className="password pb-2">
-                <label className="form__label block flex font-bold text-emerald-500" for="password">Password</label>
-                <input className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" type="password" id="password" placeholder="Password"/>
+                <label className="form__label block flex font-medium text-neutral-500">Password</label>
+                <input onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    password: e.target.value
+                  })
+                }} className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" type="password" id="password" placeholder="Password"/>
               </div>
               <div className="text-center flex items-center pb-3">
-                <input type="checkbox" id="remember" value="remember" className="mr-1.5"/>
+                <input type="checkbox" id="remember" value="remember" className="mr-1.5 cursor-pointer"/>
                 <label>Remember Me</label>
               </div>
-              <div class="footer">
-              <button type="submit" className="transition duration-75 border-2 border-sky-500 w-full px-4 py-1 rounded-md bg-sky-500 text-white">Log in</button>
+              <div className="footer">
+              <button type="button" onClick={handleLogin} className="transition duration-75 border-2 border-sky-500 w-full px-4 py-1 rounded-md bg-sky-500 text-white">Log in</button>
             </div>
           </div>
         </div>
--- src/Register.js
+++ src/Register.js
@@ -12,29 +12,29 @@ const Register=() => {
           <p className="text-stone-400 text-3xl text-center pb-2 text-black font-lato text-3xl font-semibold">Register</p>
             <div className="form-body">
                 <div className="fullname pb-2">
-                    <label className="form__label block flex font-bold text-emerald-500" for="fullName">Full Name</label>
+                    <label className="form__label block flex font-bold text-neutral-500" for="fullName">Full Name</label>
                     <input className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" type="text" id="fullName" placeholder="Full Name"/>
                 </div>
                 <div className="username pb-2">
-                    <label className="form__label block flex font-bold text-emerald-500" for="userName">Username</label>
+                    <label className="form__label block flex font-bold text-neutral-500" for="userName">Username</label>
                     <input  type="text" name="" id="userName"  className="w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" placeholder="Username"/>
                 </div>
                 <div className="email pb-2">
-                    <label className="form__label block flex font-bold text-emerald-500" for="email">Email</label>
+                    <label className="form__label block flex font-bold text-neutral-500" for="email">Email</label>
                     <input  type="email" id="email" className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" placeholder="Email"/>
                 </div>
                 <div className="password pb-2">
-                    <label className="form__label block flex font-bold text-emerald-500" for="password">Password</label>
+                    <label className="form__label block flex font-bold text-neutral-500" for="password">Password</label>
                     <input className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" type="password"  id="password" placeholder="Password"/>
                 </div>
                 <div className="confirm-password pb-3">
-                    <label className="form__label block flex font-bold text-emerald-500" for="confirmPassword">Confirm Password</label>
+                    <label className="form__label block flex font-bold text-neutral-500" for="confirmPassword">Confirm Password</label>
                     <input className="form__input w-full px-4 py-1 border-2 border-grey-500 focus-visible:outline-none rounded-lg cursor-text" type="password" id="confirmPassword" placeholder="Confirm Password"/>
                 </div>
             </div>
             <div className="text-center flex items-center pb-3">
               <input type="checkbox" id="remember" value="remember" className="mr-1.5"/>
-              <label for="remember">Remember Me</label>
+              <label for="remember" className="font-medium">Remember Me</label>
             </div>
             <div class="footer">
               <button type="submit" className="transition duration-75 border-2 border-sky-500 w-full px-4 py-1 rounded-md bg-sky-500 text-white">Register</button>
