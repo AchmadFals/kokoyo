a101f64eba2a464137841c7a0d71ea26269b04c5
AchmadFals <mangkolil5@gmail.com>
2022-04-04 09:41:14 +0700

    commit 10

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import Navbar from './Navbar';
-import menu from './menu.jpg';
+import Pulau from './pulau-padar.jpg';
+import './App.css';
 
 const Dashboard=() => {
   return <div>
             <Navbar/>
-            <nav className='navbar fixed-top navbar-expand-lg navbar-dark p-md-3'>
-              <div className='container'>
-                <a href='#' class="navbar-brand">Web Zone</a>
-              </div>
-            </nav>
-            <div>
-              <img className='bg-center' src={menu}/>
-              <div className='content text-center'>
-                <h1 className='text-black'>WEB ZONE</h1>
+            <div className='content '>
+              <img src={Pulau} className="w-full"/>
+                <p className='text-black text-left pb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                  Excepteur sint occaecat cupidatat non proident, 
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <hr className='black'/>
+                <p className='text-black text-left pb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                  Excepteur sint occaecat cupidatat non proident, 
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <hr className='text-black'/>
+                <p className='text-black text-left pb-6'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                  Excepteur sint occaecat cupidatat non proident, 
+                  sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
               </div>
             </div>
-         </div>
 };
 
 export default Dashboard;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,61 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import "./Navbar.css";
-import crop from './crop.png';
+import londrii from './londrii.png';
 
 const Navbar = () => {
+const url = useLocation()
+console.log("url", url)
   return (
-    <nav className='fixed top-0 right-0 left-0 navbar bg-hippie-blue-500 flex justify-around items-center h-20 text-white'>
-        <img src={crop} className="h-13 sm:h-50 md:h-20 lg:h-20 mr-2.5" />
-        <ul className='nav-links flex justify-end list-none w-5'>
+    <nav className='fixed top-0 right-0 left-0 navbar bg-hippie-blue-500 flex justify-around items-center h-16 text-white px-10'>
+        <img src={londrii} className="h-12 ml-16"/>
+        <ul className='flex-1 md:flex lg:flex hidden justify-end list-none w-5'>
             <Link to="/dashboard" className='manajemen_user no-underline slate-50 text-base p-2.5 m-2.5 cursor-pointer'>
-                <li>Dashboard</li>
+                <li className='relative'>
+                    Dashboard
+                    {url.pathname === "/dashboard" && (
+                        <span className="absolute bottom-[-20px] bg-orange-400 h-[4px]  text-orange-500 left-0 right-0 z-[1]"/>
+                    )}
+                </li>
             </Link>
             <Link to="/manajemen_user" className='manajemen_user no-underline slate-50 text-base p-2.5 m-2.5 cursor-pointer'>
-                <li>Manajemen User</li>
+                <li className='relative'>
+                    Manajemen User
+                    {url.pathname === "/manajemen_user" && (
+                        <span className="absolute bottom-[-20px] bg-orange-400 h-[4px] left-0 right-0 z-[1]"/>
+                    )}
+                </li>
             </Link>
             <Link to="/paket_laundry" className='paket_laundry no-underline slate-50 text-base p-2.5 m-2.5 cursor-pointer'>
-                <li>Paket Laundry</li>
+                <li className='relative'>
+                    Paket Laundry
+                    {url.pathname === "/paket_laundry" && (
+                       <span className="absolute bottom-[-20px] bg-orange-400 h-[4px] left-0 right-0 z-[1]"/> 
+                    )}
+                </li>
             </Link>
             <Link to="/tipe_pembayaran" className='tipe_pembayaran no-underline slate-50 text-base p-2.5 m-2.5 cursor-pointer'>
-                <li>Tipe Pembayaran</li>
+                <li className='relative'>
+                    Tipe Pembayaran
+                    {url.pathname === "/tipe_pembayaran" && (
+                        <span className="absolute bottom-[-20px] bg-orange-400 h-[4px] left-0 right-0 z-[1]"/>
+                    )}
+                </li>
             </Link>
             <Link to="/transaksi" className='transaksino-underline slate-50 text-base p-2.5 m-2.5 cursor-pointer'>
-                <li>Transaksi</li>
+                <li className='relative'>
+                    Transaksi
+                    {url.pathname === "/transaksi" && (
+                        <span className="absolute bottom-[-20px] bg-orange-400 h-[4px] left-0 right-0 z-[1]"/>
+                    )}
+                </li>
             </Link>
             <Link to="/register" className='laporan no-underline slate-50 text-base p-2.5 m-2.5 cursor-pointer'>
                 <li className='border-1 px-4 rounded-md border-solid border-purple-600 bg-orange-400'>Sign Up</li>
             </Link>
+        </ul>
+        <ul className='md:hidden lg:hidden visible'>
+            <li>Menu</li>
         </ul>
     </nav>
   )
