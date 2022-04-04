c513bbbfd8bf5d4a747ed0cc576e557326512798
AchmadFals <mangkolil5@gmail.com>
2022-04-04 09:42:16 +0700

    commit 10

--- src/components/Manajemen_user.js
+++ src/components/Manajemen_user.js
@@ -2,7 +2,23 @@ import React from 'react';
 import Navbar from '../Navbar';
 
 const Manajemen_user = () => {
-  return <div><Navbar/></div>
+  return  <div>
+            <Navbar/>
+            <div className='content text-center m-20'>
+              <h3 className='text-black pull-left'>Manajemen User</h3>
+            </div>
+            <div>
+              <div>
+                <h3></h3>
+              </div>
+            </div>
+            <div>
+              <p className=''>
+                Tambah Karyawan
+              </p>
+              <br />
+            </div>
+          </div>
 };
 
 export default Manajemen_user;
